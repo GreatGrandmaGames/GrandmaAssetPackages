643f46d31486751d31bec2f839c17eea0c23510f
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-02-17 14:49:34 -0500

    changed movement to take away jumping

--- Assets/GrandmaObjectCore/Positionables/Controllers/GroundMovement.cs
+++ Assets/GrandmaObjectCore/Positionables/Controllers/GroundMovement.cs
@@ -7,7 +7,9 @@ namespace Grandma
     [CreateAssetMenu(menuName = "Core/GroundMovementData")]
     public class GroundMovementData : PositionableData
     {
+        [SerializeField]
         public float speedScalar;
+        [SerializeField]
         public float jumpForce;
     }
 
@@ -31,22 +33,14 @@ namespace Grandma
             //calculate velocity
             Vector3 newVelocity = (moveHorizontal + moveVertical).normalized * groundMovementData.speedScalar;
 
-            if (InputVector.y > 0)
-            {
-                newVelocity.y = InputVector.y;
-            }
+
             return newVelocity;
         }
 
         protected override void ApplyVelocity(Vector3 velocity)
         {
-            if (velocity.y > 0)
-            {
-                rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-                rb.AddForce(new Vector3(0f, groundMovementData.jumpForce, 0f), ForceMode.Impulse);
-            }
             //rb.AddForce(velocity, ForceMode.VelocityChange);
-            rb.MovePosition(rb.position + velocity * Time.unscaledDeltaTime);
+            rb.MovePosition(rb.position + velocity * Time.deltaTime);
 
         }
 
@@ -54,7 +48,7 @@ namespace Grandma
         {
             float x = Input.GetAxisRaw("Horizontal");
             float z = Input.GetAxisRaw("Vertical");
-            int y = (Input.GetButtonDown("Jump") ? 1 : 0);
+            int y = 0;
             return new Vector3(x, y, z);
         }
     }
--- Assets/GrandmaObjectCore/Positionables/PositionableData.cs
+++ Assets/GrandmaObjectCore/Positionables/PositionableData.cs
@@ -7,8 +7,11 @@ namespace Grandma
     [CreateAssetMenu(menuName = "Core/Positionable")]
     public class PositionableData : GrandmaComponentData
     {
+        [SerializeField]
         public Vector3 position = Vector3.zero;
+        [SerializeField]
         public Vector3 rotation = Vector3.zero;
+        [SerializeField]
         public Vector3 localScale = Vector3.one;
 
         public void SetFromTransform(Transform transform)
